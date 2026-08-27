19f9f5e34a246cd9f3fcf48e72addd7f902fc4a1
Tosin <adeolamimilabake@gmail.com>
2026-08-27 16:07:30 +0100

    chapter two

--- CHAPTERTWO/INVENTORY_WAREHOUSETABLE.sql
+++ CHAPTERTWO/INVENTORY_WAREHOUSETABLE.sql
@@ -68,3 +68,56 @@ ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID
 WHERE Manager = 'Lucille Smith'
 GROUP BY WarehouseID;
 
+-- 2.51-- 
+select SKU, SKU_Description, QuantityOnHand,
+WAREHOUSE.WarehouseID,
+WarehouseCity,
+WarehouseState
+FROM INVENTORY JOIN WAREHOUSE
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID
+WHERE Manager = 'Lucille Smith';
+
+SELECT WarehouseID, sum(QuantityOnOrder) AS TotalItemsOnOrder,
+sum(QuantityOnHand) as TotalItemsOnHand
+from INVENTORY
+group by WarehouseID ,QuantityOnOrder;
+
+-- subquery is returns specific dataset to outer query  --  
+
+select SKU, SKU_Description, QuantityOnHand, QuantityOnOrder,
+WAREHOUSE.WarehouseID,
+WarehouseCity,
+WarehouseState, Manager, SquareFeet
+from INVENTORY RIGHT OUTER JOIN WAREHOUSE
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID;
+
+SELECT SKU, SKU_Description, Department
+from CATALOG_SKU_2016
+UNION
+SELECT SKU, SKU_Description, Department
+FROM CATALOG_SKU_2017;
+
+SELECT SKU, SKU_Description, Department, CatalogPage
+FROM CATALOG_SKU_2016
+UNION
+SELECT SKU, SKU_Description, Department, CatalogPage
+FROM CATALOG_SKU_2017;
+
+SELECT SKU, SKU_Description, Department
+FROM CATALOG_SKU_2016
+INTERSECT
+SELECT SKU, SKU_Description, Department
+FROM CATALOG_SKU_2017;
+
+SELECT SKU, SKU_Description, Department, CatalogPage
+FROM CATALOG_SKU_2016
+INTERSECT
+SELECT SKU, SKU_Description, Department, CatalogPage
+FROM CATALOG_SKU_2017;
+
+SELECT SKU, SKU_Description, Department
+FROM CATALOG_SKU_2016
+EXCEPT
+SELECT SKU, SKU_Description, Department
+FROM CATALOG_SKU_2017;
+
